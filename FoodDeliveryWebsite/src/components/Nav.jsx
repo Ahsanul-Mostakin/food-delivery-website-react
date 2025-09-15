c824dbd96c21d4dc93b41e316956c937ddaf92ae
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-09-15 22:18:33 +0600

    wip

--- FoodDeliveryWebsite/src/components/Nav.jsx
+++ FoodDeliveryWebsite/src/components/Nav.jsx
@@ -5,10 +5,10 @@ import { LuShoppingBag } from "react-icons/lu";
 function Nav() {
   return (
     <div className="w-full h-[100px] flex justify-between items-center px-8">
-      <div className="w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-xl">
+      <div className="w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-md">
         <MdFastfood className="w-[30px] h-[30px] text-green-500" />
       </div>
-      <form className="w-[60%] h-[60px] bg-white flex items-center px-5 gap-5 rounded-md shadow-xl ">
+      <form className="w-[70%] h-[60px] bg-white flex items-center px-5 gap-5 rounded-md shadow-xl ">
         <FaSearch className="text-green-500 w-[20px] h-[20px] " />
         <input
           className="bg-white w-[100%] outline-none text-[20px]"
@@ -16,7 +16,10 @@ function Nav() {
           placeholder="Search Items..."
         />
       </form>
-      <div className="w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-xl">
+      <div className="w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-xl relative">
+        <span className="absolute top-0 right-2 text-green-500 font-bold text-[18px]">
+          0
+        </span>
         <LuShoppingBag className="w-[30px] h-[30px] text-green-500 " />
       </div>
     </div>
