0e59784de7296db482768630e11f2d692f0a8f9d
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-09-16 03:10:02 +0600

    wip-1.44

--- FoodDeliveryWebsite/src/components/Card.jsx
+++ FoodDeliveryWebsite/src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import image1 from "../assets/image1.avif";
 import { LuLeafyGreen } from "react-icons/lu";
 import { GiChickenOven } from "react-icons/gi";
+
 function Card({ name, image, id, price, type }) {
   return (
     <div className="w-[300px] h-[400px] bg-white p-3 rounded-lg flex flex-col gap-3 shadow-lg hover:border-2 border-green-300">
@@ -13,7 +13,6 @@ function Card({ name, image, id, price, type }) {
         <div className="text-lg font-bold text-green-500">BDT {price}/-</div>
         <div className="flex justify-center items-center gap-2 text-green-500 text-lg font-semibold">
           {type === "veg" ? <LuLeafyGreen /> : <GiChickenOven />}
-
           <span>{type}</span>
         </div>
       </div>
