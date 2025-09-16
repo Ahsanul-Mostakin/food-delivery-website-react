f1c17ac6495b333b926fb043382602b6ea596298
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-09-17 05:03:14 +0600

    complete

--- src/components/Card2.jsx
+++ src/components/Card2.jsx
@@ -1,34 +1,48 @@
 import React from "react";
-import image1 from "../assets/image1.avif";
 import { RiDeleteBin5Line } from "react-icons/ri";
-const Card2 = () => {
+import { useDispatch } from "react-redux";
+import { DecrementQty, IncrementQty, RemoveItem } from "../redux/cartSlice";
+
+function Card2({ name, id, price, image, qty }) {
+  let dispatch = useDispatch();
   return (
-    <div className="w-full h-[120px] p-2 shadow-lg flex justify-between ">
+    <div className="w-full h-[120px] p-2 shadow-lg flex justify-between">
       <div className="w-[60%] h-full flex gap-5">
         <div className="w-[60%] h-full overflow-hidden rounded-lg">
-          <img src={image1} alt="" className="object-cover" />
+          <img src={image} alt="" className="object-cover" />
         </div>
-        <div className="w-[40%] h-full flex flex-col gap-5">
-          <div className="text-lg text-gray-600 font-semibold">Pancake</div>
-          <div className="w-[110px] h-[50px] bg-slate-400 flex rounded-lg overflow-hidden shadow-lg font-semibold border-2 border-green-400 text-xl">
-            <button className="w-[30%] h-full bg-white justify-center items-center text-green-400 hover:bg-gray-200">
+        <div className="w-[40%] h-full flex flex-col gap-3">
+          <div className="text-lg text-gray-600 font-semibold">{name}</div>
+          <div className="w-[110px] h-[50px] bg-slate-400 flex rounded-lg overflow-hidden shadow-lg font-semibold border-2 border-blue-400 text-xl">
+            <button
+              className="w-[30%] h-full bg-white justify-center items-center text-blue-400 hover:bg-gray-200"
+              onClick={() => (qty > 1 ? dispatch(DecrementQty(id)) : 1)}
+            >
               -
             </button>
-            <span className="w-[40%] h-full bg-slate-200 flex justify-center items-center  text-green-400">
-              1
+            <span className="w-[40%] h-full bg-slate-200 flex justify-center items-center text-blue-400">
+              {qty}
             </span>
-            <button className="w-[30%] h-full bg-white  text-green-400  hover:bg-gray-200">
+            <button
+              className="w-[30%] h-full bg-white text-blue-400 hover:bg-gray-200"
+              onClick={() => dispatch(IncrementQty(id))}
+            >
               +
             </button>
           </div>
         </div>
       </div>
-      <div className="flex flex-col justify-start items-end gap-6 ">
-        <span className="text-xl text-green-400 font-semibold">Rs 499/-</span>
-        <RiDeleteBin5Line className="text-red-400 size-6" />
+      <div className="flex flex-col justify-start items-end gap-6">
+        <span className="text-xl text-blue-400 font-semibold">
+          BDT {price}/-
+        </span>
+        <RiDeleteBin5Line
+          className="text-red-400 cursor-pointer"
+          onClick={() => dispatch(RemoveItem(id))}
+        />
       </div>
     </div>
   );
-};
+}
 
 export default Card2;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,13 @@ import { food_items } from "../food";
 import { dataContext } from "../context/UserContext";
 import { ImCross } from "react-icons/im";
 import Card2 from "../components/Card2";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const Home = () => {
   let { input, cate, setCate, showCart, setShowCart } = useContext(dataContext);
+  let items = useSelector((state) => state.cart);
 
-  // Update cate based on search input
   useEffect(() => {
     if (input === "") {
       setCate(food_items);
@@ -22,7 +24,6 @@ const Home = () => {
     }
   }, [input, setCate]);
 
-  // Filter by category
   function filter(category) {
     if (category === "All") {
       setCate(food_items);
@@ -34,52 +35,124 @@ const Home = () => {
     }
   }
 
+  let subtotal = items.reduce(
+    (total, item) => total + item.qty * item.price,
+    0
+  );
+  let deliveryFee = 20;
+  let taxes = (subtotal * 0.5) / 100;
+  let total = Math.floor(subtotal + deliveryFee + taxes);
+
   return (
     <div className="bg-slate-200 w-full min-h-screen">
       <Nav />
-      {!input ? (
+      {!input && (
         <div className="flex flex-wrap justify-center items-center gap-5 w-[100%]">
           {Categories.map((item) => (
             <div
               key={item.id}
               onClick={() => filter(item.name)}
-              className="w-[140px] h-[150px] bg-white flex flex-col items-start gap-5 p-5 justify-start text-[20px] font-semibold text-gray-600 rounded-lg shadow-xl hover:bg-green-200 cursor-pointer transition-all duration-200"
+              className="w-[140px] h-[150px] bg-white flex flex-col items-start gap-5 p-5 justify-start text-[20px] font-semibold text-gray-600 rounded-lg shadow-xl hover:bg-blue-200 cursor-pointer transition-all duration-200"
             >
               {item.icon}
               {item.name}
             </div>
           ))}
         </div>
-      ) : null}
+      )}
 
       <div className="w-full flex flex-wrap gap-5 px-5 justify-center items-center pt-8 pb-8">
-        {cate.map((item) => (
-          <Card
-            key={item.id}
-            name={item.food_name}
-            image={item.food_image}
-            price={item.price}
-            id={item.id}
-            type={item.food_type}
-          />
-        ))}
+        {cate.length > 0 ? (
+          cate.map((item) => (
+            <Card
+              key={item.id}
+              name={item.food_name}
+              image={item.food_image}
+              price={item.price}
+              id={item.id}
+              type={item.food_type}
+            />
+          ))
+        ) : (
+          <div className="text-center text-2xl text-blue-500 font-semibold pt-5">
+            No Dish Found
+          </div>
+        )}
       </div>
 
       <div
-        className={`w-[40vw] h-full fixed top-0 right-0 bg-white shadow-xl p-6 transition-all duration-500 ${
+        className={`w-full md:w-[40vw] h-full fixed top-0 right-0 bg-white shadow-xl p-6 transition-all duration-500 flex flex-col items-center overflow-auto ${
           showCart ? "translate-x-0" : "translate-x-full"
         }`}
       >
         <header className="w-full flex justify-between items-center">
-          <span className="text-green-400 text-[18px] font-semibold">
+          <span className="text-blue-400 text-[18px] font-semibold">
             Order items
           </span>
           <ImCross
-            className="w-[20px] h-[20px] cursor-pointer text-green-400 text-[18px] font-semibold hover:text-gray-600"
+            className="w-[20px] h-[20px] cursor-pointer text-blue-400 text-[18px] font-semibold hover:text-gray-600"
             onClick={() => setShowCart(false)}
           />
         </header>
-        <Card2 />
+        {items.length > 0 ? (
+          <>
+            <div className="w-full mt-9 flex flex-col gap-8">
+              {items.map((item) => (
+                <Card2
+                  key={item.id}
+                  name={item.name}
+                  price={item.price}
+                  image={item.image}
+                  id={item.id}
+                  qty={item.qty}
+                />
+              ))}
+            </div>
+            <div className="w-full border-t-2 border-gray-400 mt-7 flex flex-col gap-2 p-8"></div>
+            <div className="w-full flex justify-between items-center">
+              <span className="text-lg text-gray-600 font-semibold">
+                Subtotal
+              </span>
+              <span className="text-blue-400 font-semibold text-lg">
+                BDT {subtotal}/-
+              </span>
+            </div>
+            <div className="w-full flex justify-between items-center">
+              <span className="text-lg text-gray-600 font-semibold">
+                Delivery Fee
+              </span>
+              <span className="text-blue-400 font-semibold text-lg">
+                BDT {deliveryFee}/-
+              </span>
+            </div>
+            <div className="w-full flex justify-between items-center">
+              <span className="text-lg text-gray-600 font-semibold">Taxes</span>
+              <span className="text-blue-400 font-semibold text-lg">
+                BDT {taxes}/-
+              </span>
+            </div>
+            <div className="w-full flex justify-between items-center">
+              <span className="text-2xl text-gray-600 font-semibold">
+                Total
+              </span>
+              <span className="text-blue-400 font-semibold text-2xl">
+                BDT {total}/-
+              </span>
+            </div>
+            <button
+              className="w-[80%] p-3 bg-blue-500 rounded-lg text-white hover:bg-blue-400 transition-all"
+              onClick={() => {
+                toast.info("Order Placed...");
+              }}
+            >
+              Place Order
+            </button>
+          </>
+        ) : (
+          <div className="text-center text-2xl text-blue-500 font-semibold pt-5">
+            Empty Cart
+          </div>
+        )}
       </div>
     </div>
   );
