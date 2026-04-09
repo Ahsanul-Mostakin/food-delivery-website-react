c6f019ca38946ed63aea7063c3bead0a4634b4a1
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2026-04-09 19:34:06 +0600

    Fix server PathError

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("MongoDB connection error:", err));
 
+
 app.use("/api/auth", authRoutes);
 app.use("/api/order", orderRoutes);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
